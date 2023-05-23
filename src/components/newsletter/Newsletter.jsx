709755ee5961396e40527c9bdd7b28892c85a03b
Jeewantha <chathurajmhb7@gmail.com>
2023-05-23 08:41:11 +0530

    Complete the login process

--- src/components/newsletter/Newsletter.jsx
+++ src/components/newsletter/Newsletter.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import classes from './newsletter.module.css'
+import {AiOutlineSend} from 'react-icons/ai'
+import newsletterIllustration from '../../assets/get-newsletter-updates.svg'
 
 const Newsletter = () => {
   return (
-    <div>
-      Newsletter
-    </div>
+    <section id='contacts' className={classes.container}>
+      <div className={classes.wrapper}>
+        <h4 className={classes.subTitle}>Get our latest offers</h4>
+        <h2 className={classes.title}>Newsletter</h2>
+        <div className={classes.inputContainer}>
+          <input type='text' placeholder='Enter email...'/>
+          <AiOutlineSend className={classes.sendIcon}/>
+        </div>
+        <img src={newsletterIllustration} alt='' className={classes.illustration}/>
+      </div>
+    </section>
   )
 }
 
